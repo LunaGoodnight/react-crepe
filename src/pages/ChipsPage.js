c99ea4031767eef1790eba8b0938aef328a77778
Luna Goodnight <luna.lee@nextfortune.tw>
2021-11-30 15:19:06 +0800

    [Add] chips

--- src/pages/ChipsPage.js
+++ src/pages/ChipsPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import { chipsOptionList } from "./config/chipsOptionList";
 
 const Container = styled.div`
   border-bottom: 1px solid aqua;
@@ -19,15 +20,16 @@ const SmallChips = styled.div`
   font-size: 0.8rem;
   border-radius: 50px;
 `;
+const InputBox = styled.input`
+  &:focus {
+    outline: none;
+  }
+`;
+const DataOptions = styled.option`
+  padding: 1rem 0;
+  background: pink;
+`;
 
-const optionsList = [
-  { value: "cute" },
-  { value: "apple" },
-  { value: "banana" },
-  { value: "kiwi" },
-  { value: "purple" },
-  { value: "green" },
-];
 export const ChipsPage = () => {
   const [arr, setArr] = useState([]);
   const [input, setInput] = useState("");
@@ -35,49 +37,30 @@ export const ChipsPage = () => {
     // debugger;
     if (e.code === "Enter") {
       if (e.code === "Enter" && e.target.value.trim()) {
-        console.log(e.code);
         setArr((prev) => prev.concat(e.target.value));
         setInput("");
       }
     }
   };
-  const handleOptionsEvent = (e, value) => {
-    console.log({ value });
-    console.log({ e });
-    debugger;
-    if (e.code === "Enter") {
-      debugger;
-      // if (e.code === "Enter" && e.target.value.trim()) {
-      //   console.log(e.code);
-      //   setArr((prev) => prev.concat());
-      //   setInput("");
-      // }
-    }
-  };
   return (
     <Container>
-      {arr.map((x) => (
-        <SmallChips key={x}>{x}</SmallChips>
+      {arr.map((x, i) => (
+        <SmallChips key={i}>{x}</SmallChips>
       ))}
-      <input
+      <InputBox
         list="cute"
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
         onKeyDown={(e) => handleKeyEvent(e)}
-        // onKeyDown={(e) => setArr((prev) => prev.concat(e.target.value))}
       />
       <datalist id="cute">
-        {optionsList.map((x, i) => {
+        {chipsOptionList.map((x, i) => {
           const { value } = x;
           return (
-            <option
-              key={i}
-              onKeyDown={(e) => handleOptionsEvent(e, value)}
-              value={value}
-            >
+            <DataOptions key={i} value={value}>
               {value}
-            </option>
+            </DataOptions>
           );
         })}
       </datalist>
---
+++ src/pages/config/chipsOptionList.js
@@ -0,0 +1,8 @@
+export const chipsOptionList = [
+  { value: "cute" },
+  { value: "apple" },
+  { value: "banana" },
+  { value: "kiwi" },
+  { value: "purple" },
+  { value: "green" },
+];
