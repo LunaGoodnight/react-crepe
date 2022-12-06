e91b388e8aa8173b5e3b5e4f358635be514a3635
Luna Goodnight <luna.lee@nextfortune.tw>
2022-12-06 18:32:28 +0800

    [Add] tree

--- src/pages/ShowTree.js
+++ src/pages/ShowTree.js
@@ -1,12 +1,13 @@
 import React from "react";
-import {TreeSelector} from "../utils/tree/TreeSelector";
-import {useSweetTree} from "../hooks/useSweetTree";
-import {subsidiaryList} from "./config/subsidiaryList";
+import { TreeSelector } from "../utils/tree/TreeSelector";
+import { useSweetTree } from "../hooks/useSweetTree";
+import { subsidiaryList } from "./config/subsidiaryList";
 
 export const ShowTree = () => {
-  const treeSelect = useSweetTree({ list: subsidiaryList})
-  return <div>
-
-    <TreeSelector setIsDropdownOpen={} selectedItems={} />
-  </div>;
+  const treeSelect = useSweetTree({ list: subsidiaryList });
+  return (
+    <div>
+      <TreeSelector {...treeSelect} />
+    </div>
+  );
 };
