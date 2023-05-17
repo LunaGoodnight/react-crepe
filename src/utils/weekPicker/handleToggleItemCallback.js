31a8519e98b46b058c4d51cd9d719619c8fb5779
Luna Goodnight <luna.lee@nextfortune.tw>
2023-05-17 09:33:27 +0800

    week picker

--- src/utils/weekPicker/handleToggleItemCallback.js
+++ src/utils/weekPicker/handleToggleItemCallback.js
@@ -4,10 +4,20 @@ export const handleToggleItemCallback = ({
   index,
 }) => {
   let arr = selectedItems.slice().sort();
+
+  // 完全沒有點過的checkbox，單純加進去就好
+  // add this index
+  if (arr.length === 0) {
+    const newArray = arr.concat(index);
+    setSelectedItems(newArray);
+    return;
+  }
+  // 已經有點過
   if (arr.includes(index)) {
     const newArray = arr.filter((elementIndex) => elementIndex < index);
     setSelectedItems(newArray);
   } else {
+    // 還沒點過，如果上面有點過的，就自動補齊
     if (arr.some((ele) => ele < index)) {
       const firstElement = arr[0];
       for (let i = firstElement + 1; i <= index; i++) {
@@ -15,9 +25,13 @@ export const handleToggleItemCallback = ({
       }
       setSelectedItems(arr);
     } else {
-      // add this index
-      const newArray = arr.concat(index);
-      setSelectedItems(newArray);
+      // 還沒點過，上面也沒有點過的，就限定只能點擊上面相鄰的
+      if (arr.includes(index + 1)) {
+        const newArray = arr.concat(index);
+        setSelectedItems(newArray);
+      } else {
+        alert("只能點相鄰的周！");
+      }
     }
   }
 };
